161a869e5962a2445d50010bed75b1e36e9d5ec6
Benoit Dubuc <benoit.dubuc@toumoro.com>
2022-06-25 17:09:43 -0400

    Unicorns

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace KnpU\LoremIpsumBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getConfigTreeBuilder()
+      {
+          // TODO: Implement getConfigTreeBuilder() method.
+          $treeBuilder = new TreeBuilder();
+          $rootNode = $treeBuilder->root('knpu_lorem_ipsum');
+          $rootNode->children()
+              ->booleanNode('unicorns_are_real')->defaultTrue()->info('Whether you believe in unicorns')->end()
+              ->integerNode('min_sunshine')->defaultValue(3)->info('How much you like sunshine')->end()
+              ->scalarNode('word_provider')->defaultNull()->info('Your own word provide')->end()
+              ->end();
+          return $treeBuilder;
+
+    }
+}
